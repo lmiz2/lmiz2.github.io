3b7371c106bf8edab66043505beec2a94cd67301
lmiz2 <lmiz2@naver.com>
2020-09-07 23:04:07 +0900

    site init

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
-        "title": "test1",
-        "excerpt":"즐겨찾기  ㅎㅎㅎ  ","categories": ["java","debug"],
-        "tags": [],
-        "url": "http://localhost:4000/java/debug/test1/",
+        "title": "jekyll을 이용하여 블로그 만들어보기 (1)",
+        "excerpt":" ","categories": ["exp"],
+        "tags": ["jekyll"],
+        "url": "http://localhost:4000/exp/2243/",
         "teaser": null
       }]
